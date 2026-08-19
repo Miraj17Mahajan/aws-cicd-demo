b6e9a2a05e472e773a551b710023e00bec6279f4
Miraj17Mahajan <mohitaws17@gmail.com>
2026-08-19 13:45:52 +0530

    deploy Application version 2

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
  
 @app.route("/")
 def home():
-    return "Hello from GitHub Actions -> ECR -> EKS!"
+    return "Version 2 - Live CI/CD Deployment Successful!"
  
  
 @app.route("/health")
